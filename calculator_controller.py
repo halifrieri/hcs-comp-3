737c12fe77abf9c2ed2367e9c4b833fd695efed8
halifrieri <39467296+halifrieri@users.noreply.github.com>
2020-05-11 14:12:40 -0400

    Add files via upload

---
+++ calculator_controller.py
@@ -0,0 +1,69 @@
+###########################
+# 1. import flask library 
+# HINT: sample/request_processing.py
+###########################
+import service.calculator as calculator
+from http import client as StatusCodes
+
+from flask import Flask, request, jsonify
+
+
+###########################
+# 2. initialize your Flask application object
+# HINT: sample/explicit_application_object.py
+###########################
+app = Flask(__name__)
+
+
+
+###########################
+# 3. define route paths for the following functions with the specified path and method
+# HINT: sample/routing.py
+# 4. and parse the request to get the user_input given the request type
+# HINT: sample/request_processing.py
+###########################
+
+
+# path = '/mean', method = 'GET'
+# request type = JSON
+@app.route('/mean', methods=['GET'])
+def mean():
+	user_input = request.get_json()['input']
+	
+	results = calculator.mean(user_input)
+
+	return jsonify({'output':results}), HTTPStatus.OK
+
+
+# path = '/median', method = 'GET and POST'
+# request type = Query
+@app.route('/median', methods=['GET','POST'])
+def median():
+	user_input = request.args.get('input')
+
+	user_input = list(map(int, user_input.split(',')))
+	results = calculator.median(user_input)
+
+	return jsonify({'output':results}), HTTPStatus.OK
+
+# path = '/mode', method = 'POST'
+# request type = Form
+@app.route('/mode', methods=['POST'])
+def mode():
+	user_input = request.form.get('input')
+
+	user_input = list(map(int, user_input))
+	results = calculator.mode(user_input)
+
+	return jsonify({'output':results}), HTTPStatus.OK
+
+
+# path = '/status', method = 'GET'
+app.route('/status', methods=['GET'])
+def status():
+	result = "Application is running"
+	return result, HTTPStatus.OK
+
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=8080) 
